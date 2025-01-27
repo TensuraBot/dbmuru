17b986685708513ae9f649b870d7d58885940e14
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-01-27 20:30:44 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Selamat Datang di Rimuru Gacha</h1>
+      <h1>Yokoso di Rimuru Gacha</h1>
       <p>Temukan kartu Tensura favoritmu dan mulai bermain!</p>
       <Link href="/register">
         <button style={{ margin: '10px' }}>Daftar</button>
